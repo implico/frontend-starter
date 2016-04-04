613834f380b5098bc623fba120cd7f4f56d82ea3
implico <bartosz.sak@implico.pl>
2016-04-04 18:18:41 +0200

    task files renamed

--- gulpfile.js
+++ gulpfile.js
@@ -247,7 +247,7 @@ var tasks = {}
 //autoload tasks
 var tasksList = ['js', 'styles', 'fonts', 'sprites', 'images', 'views', 'customDirs', 'browserSync', 'clean'];
 tasksList.forEach((t) => {
-  require(dirs.tasks + t + '.task.js')(dirs, config, app, tasks);
+  require(dirs.tasks + t + '.js')(dirs, config, app, tasks);
 })
 
 
